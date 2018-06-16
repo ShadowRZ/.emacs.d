4e00d4305e023bcc6a53a365747aaf3bf9953bae
布偶君 <23130178+ShadowRZ@users.noreply.github.com>
2018-06-16 08:37:20 +0800

    Update again

--- custom.el
+++ custom.el
@@ -19,12 +19,12 @@
  '(package-selected-packages
    (quote
     (use-package cask-mode pallet company swiper flycheck counsel smartparens docbook popwin elfeed powerline moe-theme adoc-mode js2-mode web-mode nodejs-repl exec-path-from-shell)))
-  '(recentf-max-menu-item 10))
+ '(recentf-max-menu-item 10))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 98 :family "Iosevka Term"))))
+ '(default ((t (:weight normal :height 98 :family "Iosevka"))))
  '(variable-pitch ((t nil))))
 ;;; custom.el ends here
