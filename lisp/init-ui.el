637b33611d1c20d1cfff200aa3540c9639a649d6
雨宫恋叶 <23130178+ShadowRZ@users.noreply.github.com>
2019-10-06 19:50:34 +0800

    Moe theme configs.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -17,6 +17,11 @@
 (use-package moe-theme
   :ensure t
   :config
+  (setq moe-theme-highlight-buffer-id t)
+  (when (package-installed-p 'markdown-mode)
+    (setq moe-theme-resize-markdown-title '(2.0 1.7 1.5 1.3 1.0 1.0)))
+  (setq moe-theme-resize-org-title '(2.2 1.8 1.6 1.4 1.2 1.0 1.0 1.0 1.0))
+  (setq moe-theme-resize-rst-title '(2.0 1.7 1.5 1.3 1.1 1.0))
   (moe-dark)
   (moe-theme-set-color 'orange))
 
