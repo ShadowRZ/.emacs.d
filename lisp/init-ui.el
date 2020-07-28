45c89eeacff928540b2395b6085e07e14cf7f9f9
雨宫恋叶 <23130178+ShadowRZ@users.noreply.github.com>
2020-07-28 21:40:36 +0800

    Allow non-X11 only usage

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -3,13 +3,15 @@
 
 ;;; Code:
 ;; Fonts
-
-(set-fontset-font "fontset-default" '(#xe0a0 . #xe0a2) "PowerlineSymbols")
-(set-fontset-font "fontset-default" '(#xe0b0 . #xe0b3) "PowerlineSymbols")
+(if (display-graphic-p)
+    (progn
+      (set-fontset-font "fontset-default" '(#xe0a0 . #xe0a2) "PowerlineSymbols")
+      (set-fontset-font "fontset-default" '(#xe0b0 . #xe0b3) "PowerlineSymbols")))
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-(scroll-bar-mode -1)
+(if (display-graphic-p)
+     (scroll-bar-mode -1))
 
 (set-frame-parameter nil 'fullscreen 'fullboth)
 (setq-default cursor-type 'box)
