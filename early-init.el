a674d84b56484651858e4fccf96abbd192ed49af
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2023-10-29 15:28:01 +0800

    Move to use-package

--- early-init.el
+++ early-init.el
@@ -1,8 +1,5 @@
 ;;; early-init.el -*- lexical-binding: t -*-
 
-;; Enable CJK N-Gram indexing for Xapian (used by mu)
-(setenv "XAPIAN_CJK_NGRAM" "1")
-
 ;; Disable the menu bar
 (menu-bar-mode -1)
 ;; Disable the tool bar
@@ -11,28 +8,3 @@
 (scroll-bar-mode -1)
 ;; Fullscreen on startup
 (push '(fullscreen . fullboth) default-frame-alist)
-
-;; A marco for installing `PACKAGE' and do `BODY'.
-(defmacro ShadowRZ/with-eval-after-install (package &rest body)
-  "Execute BODY after PACKAGE is loaded.
-This will not automatically require PACKAGE so you can write autoloads
-without worring about requires."
-  (declare (indent defun))
-  `(progn
-     (unless (package-installed-p ,package)
-       (unless package-archive-contents
-         (package-refresh-contents))
-       (package-install ,package))
-     ,@body))
-
-;; A macro for requiring `FILE' and do `BODY'.
-(defmacro ShadowRZ/with-eval-after-require (file &rest body)
-  "Load FILE and execute BODY.
-FILE is normally a feature name, but it can also be a file name,
-in case that file does not provide any feature.  See `eval-after-load'
-for more details about the different forms of `FILE' and their semantics."
-  (declare (indent defun))
-  `(progn
-     (unless (require ,file nil 'noerror)
-       (warn "Loading %s failed" ,file))
-     ,@body))
--- init.el
+++ init.el
@@ -19,6 +19,7 @@
   (set-fontset-font t nil "Sarasa Mono CL" nil 'append)
   (set-face-attribute 'variable-pitch nil :font "Iosevka Aile Minoko"))
 
+;; Set font faces for Server frames
 (add-hook 'server-after-make-frame-hook #'ShadowRZ/set-font-faces)
 (ShadowRZ/set-font-faces)
 
@@ -39,127 +40,214 @@
            (image-type-available-p 'pbm))))
 (advice-add 'use-fancy-splash-screens-p :override #'ShadowRZ/use-fancy-splash-screen-p)
 
+;; Use Diminish to omit mode line items.
+(use-package diminish :ensure t)
+
 ;; Load a custom theme
-(ShadowRZ/with-eval-after-install 'ef-themes
-  (require 'ef-themes)
-  (setq
-   ef-themes-headings '((0 . (variable-pitch 2.2))
-			(1 . (variable-pitch 2.0))
-			(2 . (variable-pitch 1.8))
-			(3 . (variable-pitch 1.6))
-			(4 . (variable-pitch 1.4))
-			(5 . (variable-pitch 1.2))
-			(t . (variable-pitch 1.1)))
-   ef-themes-mixed-fonts t)
-  (load-theme 'ef-winter :no-confirm))
+(use-package ef-themes
+  :ensure t
+  :init
+  (setq ef-themes-headings '((0 . (variable-pitch 2.2))
+			     (1 . (variable-pitch 2.0))
+			     (2 . (variable-pitch 1.8))
+			     (3 . (variable-pitch 1.6))
+			     (4 . (variable-pitch 1.4))
+			     (5 . (variable-pitch 1.2))
+			     (t . (variable-pitch 1.1)))
+	ef-themes-mixed-fonts t)
+  :config (load-theme 'ef-winter :no-confirm))
 
 ;;; Completion framework
-(ShadowRZ/with-eval-after-install 'vertico
+(use-package vertico
+  :ensure t
   ;; Enable completion by narrowing
+  :init
   (vertico-mode t)
   (setq
    vertico-resize t
    vertico-count 6)
   ;; Improve directory navigation
-  (with-eval-after-load 'vertico
-    (define-key vertico-map (kbd "RET") #'vertico-directory-enter)
-    (define-key vertico-map (kbd "DEL") #'vertico-directory-delete-word)
-    (define-key vertico-map (kbd "M-d") #'vertico-directory-delete-char)
-    (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy)))
+  :bind (:map vertico-map
+	      ("RET" . vertico-directory-enter)
+	      ("DEL" . vertico-directory-delete-word)
+	      ("M-d" . vertico-directory-delete-char))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
 
 ;; Consult
-(ShadowRZ/with-eval-after-install 'consult
-  ;; C-x bindings (ctl-x-map)
-  (global-set-key (kbd "C-x M-:") 'consult-complex-command)     ;; orig. repeat-complex-command
-  (global-set-key (kbd "C-x b") 'consult-buffer)                ;; orig. switch-to-buffer
-  (global-set-key (kbd "C-x 4 b") 'consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-  (global-set-key (kbd "C-x 5 b") 'consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-  (global-set-key (kbd "C-x r b") 'consult-bookmark)            ;; orig. bookmark-jump
-  (global-set-key (kbd "C-x p b") 'consult-project-buffer)      ;; orig. project-switch-to-buffer
-  ;; M-g bindings (goto-map)
-  (global-set-key (kbd "M-g e") 'consult-compile-error)
-  (global-set-key (kbd "M-g f") 'consult-flymake)               ;; Alternative: consult-flycheck
-  (global-set-key (kbd "M-g g") 'consult-goto-line)             ;; orig. goto-line
-  (global-set-key (kbd "M-g M-g") 'consult-goto-line)           ;; orig. goto-line
-  (global-set-key (kbd "M-g o") 'consult-outline)               ;; Alternative: consult-org-heading
-  (global-set-key (kbd "M-g m") 'consult-mark)
-  (global-set-key (kbd "M-g k") 'consult-global-mark)
-  (global-set-key (kbd "M-g i") 'consult-imenu)
-  (global-set-key (kbd "M-g I") 'consult-imenu-multi)
-  ;; M-s bindings (search-map)
-  (global-set-key (kbd "M-s d") 'consult-find)
-  (global-set-key (kbd "M-s D") 'consult-locate)
-  (global-set-key (kbd "M-s g") 'consult-grep)
-  (global-set-key (kbd "M-s G") 'consult-git-grep)
-  (global-set-key (kbd "M-s r") 'consult-ripgrep)
-  (global-set-key (kbd "M-s l") 'consult-line)
-  (global-set-key (kbd "M-s L") 'consult-line-multi)
-  (global-set-key (kbd "M-s m") 'consult-multi-occur)
-  ;; Isearch integration
-  (global-set-key (kbd "M-s e") 'consult-isearch-history)
-  (define-key isearch-mode-map (kbd "M-s l") 'consult-line)                  ;; needed by consult-line to detect isearch
-  (define-key isearch-mode-map (kbd "M-s L") 'consult-line-multi)            ;; needed by consult-line to detect isearch
-  (add-hook 'completion-list-mode 'consult-preview-at-point-mode)
-  (setq
-   xref-show-xrefs-function #'consult-xref
-   xref-show-definitions-function #'consult-xref
-   consult-narrow-key "<"))
+(use-package consult
+  :ensure t
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+	 ("C-c M-x" . consult-mode-command)
+	 ("C-c h" . consult-history)
+	 ("C-c k" . consult-kmacro)
+	 ("C-c m" . consult-man)
+	 ("C-c i" . consult-info)
+	 ([remap Info-search] . consult-info)
+	 ;; C-x bindings in `ctl-x-map'
+	 ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+	 ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+	 ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+	 ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+	 ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+	 ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+	 ;; Custom M-# bindings for fast register access
+	 ("M-#" . consult-register-load)
+	 ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+	 ("C-M-#" . consult-register)
+	 ;; Other custom bindings
+	 ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+	 ;; M-g bindings in `goto-map'
+	 ("M-g e" . consult-compile-error)
+	 ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+	 ("M-g g" . consult-goto-line)             ;; orig. goto-line
+	 ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+	 ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+	 ("M-g m" . consult-mark)
+	 ("M-g k" . consult-global-mark)
+	 ("M-g i" . consult-imenu)
+	 ("M-g I" . consult-imenu-multi)
+	 ;; M-s bindings in `search-map'
+	 ("M-s d" . consult-find)
+	 ("M-s D" . consult-locate)
+	 ("M-s g" . consult-grep)
+	 ("M-s G" . consult-git-grep)
+	 ("M-s r" . consult-ripgrep)
+	 ("M-s l" . consult-line)
+	 ("M-s L" . consult-line-multi)
+	 ("M-s k" . consult-keep-lines)
+	 ("M-s u" . consult-focus-lines)
+	 ;; Isearch integration
+	 ("M-s e" . consult-isearch-history)
+	 :map isearch-mode-map
+	 ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+	 ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+	 ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+	 ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+	 ;; Minibuffer history
+	 :map minibuffer-local-map
+	 ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+	 ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+	register-preview-function #'consult-register-format)
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+	xref-show-definitions-function #'consult-xref)
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+)
 
 ;; Orderless
-(ShadowRZ/with-eval-after-install 'orderless
-  (require 'orderless)
-  (setq
-   completion-styles '(orderless basic)
-   completion-category-overrides '((file (styles basic partial-completion)))))
+(use-package orderless
+  :ensure t
+  :init
+  (setq completion-styles '(orderless basic)
+	completion-category-defaults nil
+	completion-category-overrides '((file (styles partial-completion)))))
 
 ;; Marginalia
-(ShadowRZ/with-eval-after-install 'marginalia
+(use-package marginalia
+  :ensure t
+  :init
   (marginalia-mode))
 
-;; Embark
-(ShadowRZ/with-eval-after-install 'embark
-  (global-set-key (kbd "C-.") 'embark-act)
-  (global-set-key (kbd "M-.") 'embark-dwim)
-  (global-set-key (kbd "C-h B") 'embark-bindings)
+(use-package embark
+  :ensure t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("M-." . embark-dwim)
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
 	       '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
 		 nil
-		 (window-parameters (mode-line-format . none))))
-  ;; Embark + Consult
-  (ShadowRZ/with-eval-after-install 'embark-consult
-    (require 'embark-consult)))
+		 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :after (embark consult)
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 ;;; Pop-up auto-completion
-(ShadowRZ/with-eval-after-install 'company
-  ;; Enable Company by default in programming buffers
-  (add-hook 'prog-mode-hook #'company-mode))
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  ;; (corfu-separator ?\s)          ;; Orderless field separator
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+  ;; Recommended: Enable Corfu globally.
+  ;; This is recommended since Dabbrev can be used globally (M-/).
+  ;; See also `global-corfu-modes'.
+  :init
+  (global-corfu-mode))
 
 ;;; Git client
-(ShadowRZ/with-eval-after-install 'magit
+(use-package magit
+  :ensure t
   ;; Bind the `magit-status' command to a convenient key.
-  (global-set-key (kbd "C-c g") #'magit-status)
+  :bind (("C-c g" . magit-status))
+  :config
   ;; Show word-granularity differences within diff hunks
   (setq magit-diff-refine-hunk t))
 
 ;;; Indication of local VCS changes
-(ShadowRZ/with-eval-after-install 'diff-hl
+(use-package diff-hl
   ;; Enable `diff-hl' support by default in programming buffers
-  (add-hook 'prog-mode-hook #'diff-hl-mode)
+  :hook ((prog-mode . diff-hl-mode))
+  :config
   ;; Update the highlighting without saving
   (diff-hl-flydiff-mode t))
 
 ;;; Markdown support
-(ShadowRZ/with-eval-after-install 'markdown-mode)
+(use-package markdown-mode
+  :ensure t)
 
 ;;; Outline-based notes management and organizer
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
+(use-package org
+  :bind (("C-c l" . org-store-link)
+	 ("C-c a" . org-agenda)))
 
 ;;; Additional Org-mode related functionality
-(ShadowRZ/with-eval-after-install 'org-contrib)
+(use-package org-contrib
+  :ensure t
+  :after (org))
 
 ;;; Which-key
-(ShadowRZ/with-eval-after-install 'which-key
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :init
   (which-key-mode))
 
 (setq window-resize-pixelwise t)
@@ -169,16 +257,10 @@
 (recentf-mode t)
 
 ;; Emacs Pinentry
-(ShadowRZ/with-eval-after-install 'pinentry
-  (require 'pinentry)
+(use-package pinentry
+  :ensure t
+  :init
   (pinentry-start))
-
-;; Use Diminish to omit mode line items.
-(ShadowRZ/with-eval-after-install 'diminish
-  (require 'diminish)
-  (add-hook 'company-mode-hook (lambda ()
-				 (diminish 'company-mode)
-				 (diminish 'which-key-mode))))
 
 ;; Store automatic customisation options elsewhere
 (setq custom-file (locate-user-emacs-file "custom.el"))
